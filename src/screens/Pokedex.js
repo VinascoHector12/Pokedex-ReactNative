030ee771504ddf74053ff547d26960d90cf244d1
Hector Vinasco <hectorvinasco12@gmail.com>
2022-07-21 17:23:20 -0500

    Fetch al api de pokemones e impresion en consola

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,6 +1,23 @@
 import { View, Text } from 'react-native'
+import { useState, useEffect } from 'react'
+import { getPokemonsApi } from '../api/pokemon'
 
 export default function Pokedex() {
+  useEffect(() => {
+    (async ()=>{//Funcion anonima autoejecutable
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <View>
       <Text>Pokedex</Text>
