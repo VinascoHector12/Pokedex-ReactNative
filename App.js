341ae6df63d712ef9a8b78868e2811915cefc355
Hector Vinasco <hectorvinasco12@gmail.com>
2022-07-21 10:22:58 -0500

    Barra de navegación con pokeball

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 //import 'react-native-gesture-handler'
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import Navigation from './src/navigation/Navigation'
 
@@ -10,4 +11,11 @@ export default function App() {
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,4 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import { Image } from 'react-native'
+
 import FavoriteScreen from '../screens/Favorite'
 import PokedexScreen from '../screens/Pokedex'
 import AccountScreen from '../screens/Account'
@@ -8,9 +11,59 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <Tab.Navigator>
-        <Tab.Screen name='Favorite' component={ FavoriteScreen } />
-        <Tab.Screen name='Pokedex' component={ PokedexScreen } />
-        <Tab.Screen name='Account' component={ AccountScreen } />
+        <Tab.Screen 
+            name='Favoritos' 
+            component={ FavoriteScreen } 
+            options={{
+                tabBarLabel: "Favoritos",
+                tabBarIcon: ({ color, size }) => ( 
+                    <Icon name='heart' color={color} size={size} /> 
+                ),
+            }}
+        />
+        <Tab.Screen 
+            name='Pokedex' 
+            component={ PokedexScreen } 
+            options={{
+                tabBarLabel: "",
+                tabBarIcon: () => renderPokeball()
+            }}
+        />
+        <Tab.Screen 
+            name='Mi cuenta' 
+            component={ AccountScreen } 
+            options={{
+                tabBarLabel: "Mi cuenta",
+                tabBarIcon: ({ color, size }) => ( 
+                    <Icon name='user' color={color} size={size} /> 
+                )
+            }}
+        />
     </Tab.Navigator>
   );
 }
+
+function renderPokeball(){
+    return(
+        <Image 
+            source={require('../assets/pokeball.png')}
+            style={{ width: 75, height: 75, top: -15 }}
+        />
+    );
+}
+
+/*
+Bottom Tab Navigator: 'tabBarOptions' is deprecated. Migrate the options to 'screenOptions' instead.
+
+Place the following in 'screenOptions' in your code to keep current behavior:
+
+{
+  "tabBarShowLabel": false,
+  "tabBarStyle": [
+    {
+      "display": "flex"
+    },
+    null
+  ]
+}
+*/
