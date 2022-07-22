e80b03baeb17d47cd0c25e25839d2911c67b6f73
Hector Vinasco <hectorvinasco12@gmail.com>
2022-07-22 09:59:24 -0500

    Uso de efect y state para manejo de pokemons

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,11 @@
 import { View, Text } from 'react-native'
 import { useState, useEffect } from 'react'
-import { getPokemonsApi } from '../api/pokemon'
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from '../api/pokemon'
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log('pokemons---->', pokemons)
+
   useEffect(() => {
     (async ()=>{//Funcion anonima autoejecutable
       await loadPokemons();
@@ -12,7 +15,21 @@ export default function Pokedex() {
   const loadPokemons = async () => {
     try {
       const response = await getPokemonsApi();
-      console.log(response);
+      //console.log(response);
+
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          imagen: pokemonDetails.sprites.other['official-artwork'].front_default
+        });
+      }
+      setPokemons([...pokemons, ...pokemonsArray]);
+
     } catch (error) {
       console.error(error);
     }
